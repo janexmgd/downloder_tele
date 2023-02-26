db141b6339fb87f460b16900d00463ddfb52c369
janexmgd <janexmgd@gmail.com>
2023-02-26 11:40:59 +0700

    some fix

--- index.js
+++ index.js
@@ -1,91 +1,88 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
-const axios = require('axios');
-const download = require('download');
-const fs = require('fs');
-const path = require('path');
+const { default: axios } = require('axios');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const download = require('download');
+const { Telegraf } = require('telegraf');
+const fs = require('fs');
+const config = require('./app/config/config');
+const bot = new Telegraf(config.telegram.token);
 bot.on('text', async (ctx) => {
-  // await ctx.reply(ctx.message.text);
   try {
-    console.log('get messsage');
-    let link = ctx.message.text;
+    const link = ctx.message.text;
     let res = await axios.post('https://ssyoutube.com/api/convert', {
       url: `${link}`,
     });
-
-    // const willDownload = res.data.url[0].url;
+    // console.log(res.data.url);
     if (res.data.length) {
-      for (let i = 0; i <= res.data.length; i++) {
-        let willDownload = res.data[i].url[0].url;
-        // Url of the image
-        const urlFile = willDownload;
-        // Path at which image will get downloaded
+      for (let i = 0; i < res.data.length; i++) {
+        let urlDownload = res.data[i].url[0].url;
+        const urlFile = urlDownload;
         const filePath = `${__dirname}/media`;
         let fileName;
-        download(urlFile, filePath)
-          .then(() => {
-            //passsing directoryPath and callback function
-            fs.readdir(filePath, function (err, files) {
-              //handling error
-              if (err) {
-                return console.log('Unable to scan directory: ' + err);
-              }
-              //listing all files using forEach
-              files.forEach(function (file) {
-                // Do whatever you want to do with the file
-                fileName = file;
-                ctx
-                  .replyWithPhoto({ source: `${filePath}/${fileName}` })
-                  .then(() => {
-                    fs.unlinkSync(`${filePath}/${fileName}`);
-                  });
-              });
+        download(urlFile, filePath).then(() => {
+          fs.readdir(filePath, (err, files) => {
+            if (err) {
+              console.log('unable to scan directory');
+            }
+            files.forEach(function (file) {
+              // Do whatever you want to do with the file
+              fileName = file;
+              ctx
+                .replyWithPhoto({ source: `${filePath}/${fileName}` })
+                .then(() => {
+                  fs.unlinkSync(`${filePath}/${fileName}`);
+                });
             });
-          })
-          .catch((err) => {
-            console.log(err);
           });
+        });
+      }
+    } else {
+      let urlDownload = res.data.url[0].url;
+      const urlFile = urlDownload;
+      const filePath = `${__dirname}/media`;
+      let fileName;
+      // return console.log(res.data.url[0].ext);
+      // cek ekstensi file
+      if (res.data.url[0].ext == 'jpg') {
+        // console.log('jpg');
+        download(urlFile, filePath).then(() => {
+          fs.readdir(filePath, (err, files) => {
+            if (err) {
+              console.log('unable to scan directory');
+            }
+            files.forEach(function (file) {
+              // Do whatever you want to do with the file
+              fileName = file;
+              ctx
+                .replyWithPhoto({ source: `${filePath}/${fileName}` })
+                .then(() => {
+                  fs.unlinkSync(`${filePath}/${fileName}`);
+                });
+            });
+          });
+        });
+      } else {
+        // return console.log('mp4');
+        download(urlFile, filePath).then(() => {
+          fs.readdir(filePath, (err, files) => {
+            if (err) {
+              console.log('unable to scan directory');
+            }
+            files.forEach(function (file) {
+              // Do whatever you want to do with the file
+              fileName = file;
+              ctx
+                .replyWithVideo({ source: `${filePath}/${fileName}` })
+                .then(() => {
+                  fs.unlinkSync(`${filePath}/${fileName}`);
+                });
+            });
+          });
+        });
       }
     }
-    // console.log(res.data);
-    // await ctx.sendMessage(JSON.stringify(res.data));
-
-    // // Url of the image
-    // const urlFile = willDownload;
-    // // Path at which image will get downloaded
-    // const filePath = `${__dirname}/media`;
-    // let fileName;
-    // download(urlFile, filePath)
-    //   .then(() => {
-    //     //passsing directoryPath and callback function
-    //     fs.readdir(filePath, function (err, files) {
-    //       //handling error
-    //       if (err) {
-    //         return console.log('Unable to scan directory: ' + err);
-    //       }
-    //       //listing all files using forEach
-    //       files.forEach(function (file) {
-    //         // Do whatever you want to do with the file
-    //         fileName = file;
-    //         ctx
-    //           .replyWithPhoto({ source: `${filePath}/${fileName}` })
-    //           .then(() => {
-    //             fs.unlinkSync(`${filePath}/${fileName}`);
-    //           });
-    //       });
-    //     });
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
   } catch (error) {
-    console.log(error.message);
+    ctx.reply(error.message);
   }
 });
+console.log('bot launch');
 bot.launch();
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
