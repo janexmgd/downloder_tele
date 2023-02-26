73ee075192839f17b0bda3ea4f1be28066792f25
janexmgd <janexmgd@gmail.com>
2023-02-26 11:57:30 +0700

    adding config

---
+++ app/config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+module.exports = {
+  // Token
+  telegram: {
+    // from @botfather on telegram
+    token: process.env.BOT_TOKEN,
+  },
+  // Debug
+  debug: true,
+};
